0f8bf32f7470015effa9d2c03c26aecc5d070505
marvin <marvin@marvinisaac.com>
2020-08-06 09:59:54 +0800

    :tada: Initial setup

---
+++ src/app/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="hero is-fullheight">
+        <main class="hero-body">
+            <router-view></router-view>
+        </main>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.hero-body {
+    padding: 0;
+}
+</style>
